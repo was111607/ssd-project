43f200b692d5842a07a517dda4ed151e48a862e2
William Sewell <williamsewellart@gmail.com>
2020-01-20 04:27:42 +0000

    completed existence checker and appending rows to a tsv

--- pre_process.py
+++ pre_process.py
@@ -1,5 +1,8 @@
 import os
 import re
+import csv
+import math
+import linecache as lc
 import tweepy as tw # Installed via pip
 import pandas as pd
 api_key = "QOp19hmqlyZ4DcU388vHtFUsX"
@@ -24,34 +27,71 @@ api = tw.API(auth, wait_on_rate_limit=True)
 # Repeat until all tweet id's have been iterated through
 #
 # Different data items for each image bc it may provide a different result when put through the model
+
+def searchId(lPoint, rPoint, target): # target is int
+    while (lPoint <= rPoint):
+        mPoint = int(math.floor(lPoint + ((rPoint - lPoint) / 2)))
+        line = lc.getline("t4sa_text_sentiment.tsv", mPoint).rstrip()
+        id = int(line.split()[0])
+        # print(id)
+        # print(target)
+        # print(lPoint)
+        # print(mPoint)
+        # print(rPoint)
+        # print("\n")
+        if (id == target):
+            return line
+        elif id < target:
+            lPoint = mPoint + 1
+        else:
+            rPoint = mPoint - 1
+        #input()
+    return -1 # not found
+
+    # with open("t4sa_text_sentiment.tsv") as readFile:
+    #     reader = csv.reader(readFile, delimiter = "\t")
+    #     next(reader) # Skips header row
+    #     while (lPoint <= rPoint):
+    #         mPoint = int(math.floor(lPoint + ((rPoint - lPoint) / 2)))
+    #         if ()
+
+#### Figure out a way to collate text content (from raw_tweets use bin search?) and image sentiments into 1 row.
+### write 3 csvs, 1 text 1 image 1 both
 def existenceCheck(api, idList):
+    existingIds = []
     tweets = api.statuses_lookup(idList)
     for tweet in tweets:
-        print(tweet.id)
+        existingIds.extend([tweet.id_str])
+    return existingIds
 
 def main():
     idList = []
+    existingIds = []
+
     with open("test_bt4sa.txt", "r") as readFile:
-        # try:
         for line in readFile:
             id = re.search(r'(?<=/)\w+(?=-)', line)
             #print(id.group(0))
             idList.extend([id.group(0)])
-            if (len(idList) == 5): #100
-                existenceCheck(api, idList)
+            if (len(idList) == 100): # change to 100
+                existingIds.extend(existenceCheck(api, idList))
                 idList.clear()
-                print("\n")
+                print(f"existingIds length: {len(existingIds)}")
         if (len(idList) > 0):
             # lookup
-            existenceCheck(api, idList)
+            existingIds.extend(existenceCheck(api, idList))
+            print(f"existingIds length: {len(existingIds)}")
         readFile.close()
 
     # bin search in other file for this ting
-
-    # with open("test_t4sa.tsv", "w") as writeFile:
-    #     writer = csv.writer(writeFile, delimiter = "\t")
-    #     for line in sortedList:
-    #         writer.writerow(line)
-    #     writeFile.close()
+    count = 0
+    with open("test_write.tsv", "w") as writeFile:
+        writer = csv.writer(writeFile, delimiter = "\t")
+        for id in existingIds:
+            result = searchId(1, 1179957, int(id))
+            writer.writerow(result.split()) # 1 skips header
+            count += 1
+            print(f"number written into tsv: {count}")
+        writeFile.close()
 if __name__ == "__main__":
     main()
