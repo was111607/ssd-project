437b40b93c0d0cc549e1f9a65e80c38e57ac1c23
William Sewell <williamsewellart@gmail.com>
2020-04-07 14:55:53 +0100

    lstm dropout text model

--- text_process.py
+++ text_process.py
@@ -323,7 +323,7 @@ def main():
     testFile = "/b-t4sa/model_input_testing.csv"
     isAws = True
     if isAws is True:
-        os.environ["CUDA_VISIBLE_DEVICES"] = "2" # Set according to CPU to use
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1" # Set according to CPU to use
         trainFile = awsDir + trainFile
         valFile = awsDir + valFile
         testFile = awsDir + testFile
