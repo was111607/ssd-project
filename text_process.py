5e2b0597cdf30e8dec6d82debfaf9b19e4dd997b
William Sewell <williamsewellart@gmail.com>
2020-04-09 18:22:07 +0100

    d_lstm_dropout

--- text_process.py
+++ text_process.py
@@ -168,7 +168,7 @@ def textModel():# (dRate = 0.0): # (lr = 0.0, mom = 0.0): # (dRate = 0.0)
 #    print(model.summary())
     return model
 
-def decisionModel(dRate = 0.0)#(lr = 0.0, mom = 0.0):
+def decisionModel(dRate = 0.0): #(lr = 0.0, mom = 0.0):
     with open("./training_counter.pickle", "rb") as readFile:
         tokeniser = pickle.load(readFile)
         maxVocabSize = len(tokeniser) + 1 # ~ 120k
