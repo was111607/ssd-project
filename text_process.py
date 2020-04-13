d4009d582ec6d7d62ff50e47ad5fd13e459739fe
William Sewell <williamsewellart@gmail.com>
2020-04-13 02:44:24 +0100

    d_h3_dropouts_2h

--- text_process.py
+++ text_process.py
@@ -326,7 +326,7 @@ def main():
     testFile = "/b-t4sa/model_input_testing.csv"
     isAws = True
     if isAws is True:
-        os.environ["CUDA_VISIBLE_DEVICES"] = "1" # Set according to CPU to use
+        os.environ["CUDA_VISIBLE_DEVICES"] = "0" # Set according to CPU to use
         trainFile = awsDir + trainFile
         valFile = awsDir + valFile
         testFile = awsDir + testFile
@@ -477,14 +477,14 @@ def main():
     # summariseResults(results)
     # saveResults("d_lr_0075", results, isAws)
 
-    dropout = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5] #[0.5, 0.6, 0.7, 0.8, 0.9]
+    dropout = [0.5, 0.6, 0.7, 0.8, 0.9]
     paramGrid = dict(dRate = dropout)
     dModel = keras.wrappers.scikit_learn.KerasClassifier(build_fn = decisionModel, verbose = 1, epochs = 5, batch_size = 16)
     grid = GridSearchCV(estimator = dModel, param_grid = paramGrid, n_jobs = 1, cv = 3)
     XCombined = np.array([[XTrain[i], trainImgClass[i]] for i in range(XTrain.shape[0])])
     results = grid.fit(XCombined, to_categorical(YTrain))
     summariseResults(results)
-    saveResults("d_h3_dropout", results, isAws)
+    saveResults("d_h3_dropout_2h", results, isAws)
 
     # fModel = featureModel()
     # fLogger = CSVLogger(dir + "/feature_log.csv", append = False, separator = ",")
