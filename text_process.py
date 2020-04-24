df9ef87840f77c40f2e355780ac0957027e1d6c4
William Sewell <williamsewellart@gmail.com>
2020-04-24 09:17:09 +0100

    Image sentiment model training

--- text_process.py
+++ text_process.py
@@ -94,7 +94,7 @@ def scheduledLr(epoch):
         return initialLr / (10 ** decayStep)
 
 def t4saVGG(mainPath): # evaluate gen
-    vgg19 = VGG19(weights = "./vgg19_ft_weights.h5", include_top = False)
+    vgg19 = VGG19(weights = path.join(mainPath, "vgg19_ft_weights.h5"), include_top = False)
     model = Sequential()
     layerNames = ["conv1_1",
         "conv1_2",
