0ff8b37a1cbff69f11d30cd78a27aca236087f38
William Sewell <williamsewellart@gmail.com>
2020-04-24 10:04:33 +0100

    Image sentiment model training

--- text_process.py
+++ text_process.py
@@ -132,11 +132,11 @@ def t4saVGG(mainPath): # evaluate gen
             if (hasattr(layer, attribute) is True) and (layer.trainable is True):
                 print("regs set")
                 setattr(layer, attribute, regulariser)
-    modelJson = model.to_json()
+    # modelJson = model.to_json()
     dir = path.join(mainPath, "VGG_ft_structure.json")
     # Reload json to implement change in regularizers
     model.save_weights("yes.h5")
-    model = model_from_json(modelJson)
+    model = model_from_json(model.to_json())
     #model.load_weights("yes.h5")
     # with open(dir, "w") as writeJson:
     #     writeJson.write(modelJson)
