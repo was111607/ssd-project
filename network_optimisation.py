657427e3f6f618ae6cdc13b0c63efbea34b3f28a
William Sewell <williamsewellart@gmail.com>
2020-05-01 08:14:55 +0100

    rec_dropouts gs

--- network_optimisation.py
+++ network_optimisation.py
@@ -53,7 +53,7 @@ def main():
     curDir = "."
     isAws = True
     if isAws is True:
-        os.environ["CUDA_VISIBLE_DEVICES"] = "2" # Set according to CPU to use
+        os.environ["CUDA_VISIBLE_DEVICES"] = "1" # Set according to CPU to use
         mainPath = awsDir
     else:
         mainPath = curDir
